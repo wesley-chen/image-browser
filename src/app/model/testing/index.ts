e37e15aebcd69d197075a6bd914d34c1473c846c
Wesley Chen <topolog@gmail.com>
2016-12-27 01:30:03 +0800

    Remove testing codes

--- src/app/model/testing/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './fake-hero.service';
-
-
-/*
-Copyright 2016 Google Inc. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/

5b8241de3a8fd82083054aacc55c0410d787418b
Wesley Chen <topolog@gmail.com>
2017-01-05 02:54:38 +0800

    Center container bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import 'bootstrap/dist/css/bootstrap.css';
+import '../assets/css/navbar-fixed-side.css';
 import '../assets/css/styles.css';
 
 @Component({

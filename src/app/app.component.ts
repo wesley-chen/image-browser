2fd32783e4d469b4306cd7ec6dce6d42d129a904
Wesley Chen <topolog@gmail.com>
2017-01-07 20:15:50 +0800

    Clean up sidebar css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
-import '../assets/css/navbar-fixed-side.css';
 import '../assets/css/styles.css';
-
 
 
 @Component({

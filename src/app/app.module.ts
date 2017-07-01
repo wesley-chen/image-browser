c021c9d6b06526bf6be292be432b89c43651ba37
Wesley Chen <topolog@gmail.com>
2017-07-01 18:06:38 +0800

    Upgrade to angular 4.2.5 and angular-material 2.0.0-beta.8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule, MdIconRegistry } from '@angular/material';
+
 
 import { AppComponent } from './app.component';
 
@@ -11,7 +13,7 @@ import { ImageContainerComponent } from './image-container';
 
 @NgModule({
   imports: [
-    BrowserModule, FlexLayoutModule.forRoot(), MaterialModule.forRoot()
+    BrowserModule, BrowserAnimationsModule, FlexLayoutModule, MaterialModule
   ],
   declarations: [AppComponent, ImageBrowserComponent, ImageGridComponent, ImageContainerComponent],
   providers: [MdIconRegistry],
--- src/vendor.ts
+++ src/vendor.ts
@@ -6,16 +6,16 @@ import '@angular/http';
 import '@angular/platform-browser';
 import '@angular/platform-browser-dynamic';
 import '@angular/router';
+import 'hammerjs'
 
 // RxJS
 import 'rxjs';
 // Other vendors for example jQuery, Lodash or Bootstrap
 // You can import js, ts, css, sass, ...
 
-//import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 import 'material-design-icons/iconfont/material-icons.css';
-//import '@angular/material/core/theming/prebuilt/deeppurple-amber.css';
-import '@angular/material/core/theming/prebuilt/indigo-pink.css';
+//import "@angular/material/prebuilt-themes/deeppurple-amber.css"
+import "@angular/material/prebuilt-themes/indigo-pink.css"
 //import 'material-components-web/dist/material-components-web.css';
 
